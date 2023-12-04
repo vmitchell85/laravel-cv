7d74cce881dfef67ae9f524da458c379fa646786
Vince Mitchell <vincent.mitchell@gmail.com>
2023-12-04 10:08:41 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,14 @@
                         </a>
                         <a href="https://twitter.com/vmitchell85" title="Twitter">
                             <svg class="w-8 h-8 hover:text-orange-600 text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.31 18.25C14.7819 18.25 17.7744 13.4403 17.7744 9.26994C17.7744 9.03682 17.9396 8.83015 18.152 8.73398C18.8803 8.40413 19.8249 7.49943 18.8494 5.97828C18.2031 6.32576 17.6719 6.51562 16.9603 6.74448C15.834 5.47393 13.9495 5.41269 12.7514 6.60761C11.9785 7.37819 11.651 8.52686 11.8907 9.62304C9.49851 9.49618 6.69788 7.73566 5.1875 5.76391C4.39814 7.20632 4.80107 9.05121 6.10822 9.97802C5.63461 9.96302 5.1716 9.82741 4.75807 9.58305V9.62304C4.75807 11.1255 5.75654 12.4191 7.1444 12.7166C6.70672 12.8435 6.24724 12.8622 5.80131 12.771C6.19128 14.0565 7.87974 15.4989 9.15272 15.5245C8.09887 16.4026 6.79761 16.8795 5.45806 16.8782C5.22126 16.8776 4.98504 16.8626 4.75 16.8326C6.11076 17.7588 7.69359 18.25 9.31 18.2475V18.25Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path
+                                    d="M9.31 18.25C14.7819 18.25 17.7744 13.4403 17.7744 9.26994C17.7744 9.03682 17.9396 8.83015 18.152 8.73398C18.8803 8.40413 19.8249 7.49943 18.8494 5.97828C18.2031 6.32576 17.6719 6.51562 16.9603 6.74448C15.834 5.47393 13.9495 5.41269 12.7514 6.60761C11.9785 7.37819 11.651 8.52686 11.8907 9.62304C9.49851 9.49618 6.69788 7.73566 5.1875 5.76391C4.39814 7.20632 4.80107 9.05121 6.10822 9.97802C5.63461 9.96302 5.1716 9.82741 4.75807 9.58305V9.62304C4.75807 11.1255 5.75654 12.4191 7.1444 12.7166C6.70672 12.8435 6.24724 12.8622 5.80131 12.771C6.19128 14.0565 7.87974 15.4989 9.15272 15.5245C8.09887 16.4026 6.79761 16.8795 5.45806 16.8782C5.22126 16.8776 4.98504 16.8626 4.75 16.8326C6.11076 17.7588 7.69359 18.25 9.31 18.2475V18.25Z"
+                                    stroke="currentColor"
+                                    stroke-width="1.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
                             </svg>
-
                         </a>
                     </div>
                 </div>
@@ -28,39 +33,26 @@
         <div class="max-w-5xl mx-auto my-12 px-2">
             <h2 class="mb-6">Introduction</h2>
             <div class="prose max-w-none text-sm prose-a:text-orange-600 prose-a:font-normal prose-a:no-underline">
-                <p>
-                    From High School I always had an interest in the internet and developing websites. After taking a course at my career center I got hooked and started building sites using HTML and CSS. First I used Frontpage, then RapidWeaver, and eventually learned about Wordpress. Somewhere in the mix I also learned some Adobe Flash and built some websites and soundboards using that. My desire to build for the web was always there just mostly in a freelance context.
-                </p>
-                <p>
-                    Eventually I made it to and through DeVry and got a job doing miscellaneous IT tasks for a small company. While working for this company a buddy told me about this development framework called CodeIgniter and said I should learn it and I could build much more with it than I could with Wordpress. I was interested but didn't really have the time to check it out. A few months later that same friend would introduce me to Laravel 5.
-                </p>
-                <p>
-                    Ever since learning about Laravel 5 and especially Jeffery Way's generators I was hooked. I started watching everything Laracasts put out, listening to all the podcasts available, and even making a <a href="https://github.com/laravel/framework/commit/ecfd0c45fdff20beb590ff443b65c6c7abd34269">small contribution</a> to the Laravel framework. I eventually convinced my boss to let me create some internal applications using Laravel.
-                </p>
-                <p>
-                    My love for building websites and applications with Laravel grew so much I decided to pursue a career as a web developer instead of sticking with my IT position. Now I manage a small team of developers and several proprietary Laravel applications and packages.
-                </p>
+                <p>From High School I always had an interest in the internet and developing websites. After taking a course at my career center I got hooked and started building sites using HTML and CSS. First I used Frontpage, then RapidWeaver, and eventually learned about Wordpress. Somewhere in the mix I also learned some Adobe Flash and built some websites and soundboards using that. My desire to build for the web was always there just mostly in a freelance context.</p>
+                <p>Eventually I made it to and through DeVry and got a job doing miscellaneous IT tasks for a small company. While working for this company a buddy told me about this development framework called CodeIgniter and said I should learn it and I could build much more with it than I could with Wordpress. I was interested but didn't really have the time to check it out. A few months later that same friend would introduce me to Laravel 5.</p>
+                <p>Ever since learning about Laravel 5 and especially Jeffery Way's generators I was hooked. I started watching everything Laracasts put out, listening to all the podcasts available, and even making a <a href="https://github.com/laravel/framework/commit/ecfd0c45fdff20beb590ff443b65c6c7abd34269">small contribution</a> to the Laravel framework. I eventually convinced my boss to let me create some internal applications using Laravel.</p>
+                <p>My love for building websites and applications with Laravel grew so much I decided to pursue a career as a web developer instead of sticking with my IT position. Now I manage a small team of developers and several proprietary Laravel applications and packages.</p>
             </div>
         </div>
         <div class="max-w-5xl mx-auto my-12 px-2">
             <h2 class="mb-6">Cover Letter</h2>
             <div class="prose max-w-none text-sm prose-a:text-orange-600 prose-a:font-normal prose-a:no-underline">
-                <p>
-                    When I look at my motivation and desire to help others learn and grow and compare that with the values of the Laravel organization and the commitment you all have to the community I can't help but want to work at Laravel. I know my experience with Laravel, VueJS, Tailwind CSS, managing servers, leading development teams, and other skills will come in handy.
-                </p>
-                <p>
-                    On the other hand I also know that this would be quite the learning experience for me. I don't know all the inner workings of Laravel, nor do I fully understand some of the more advanced testing techniques (even though I've watched Adam Wathan's course like 5 times). What I can say is that if hired, I would be overjoyed to learn and grow. Working hard to support the existing applications and services, and give back to the community.
-                </p>
+                <p>When I look at my motivation and desire to help others learn and grow and compare that with the values of the Laravel organization and the commitment you all have to the community I can't help but want to work at Laravel. I know my experience with Laravel, VueJS, Tailwind CSS, managing servers, leading development teams, and other skills will come in handy.</p>
+                <p>On the other hand I also know that this would be quite the learning experience for me. I don't know all the inner workings of Laravel, nor do I fully understand some of the more advanced testing techniques (even though I've watched Adam Wathan's course like 5 times). What I can say is that if hired, I would be overjoyed to learn and grow. Working hard to support the existing applications and services, and give back to the community.</p>
             </div>
         </div>
         <div class="max-w-5xl mx-auto my-12 px-2">
             <h2 class="mb-6">Projects</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 text-sm">
-                <div><a class="text-orange-600" href="https://github.com/dsg-innosource/laravel-app-manager">Laravel Application Manager</a></div>
-                <div><a class="text-orange-600" href="https://ohiolaravel.com/">Ohio Laravel Organizer</a></div>
-                <div><a class="text-orange-600" href="https://calumetchristianschool.org/">Calumet Christian School <small>(Statamic)</small></a></div>
+                <div>
+                    <a class="text-orange-600" href="https://calumetchristianschool.org/">Calumet Christian School <small>(Statamic)</small></a>
+                </div>
                 <div><a class="text-orange-600" href="https://github.com/vmitchell85/alforge">AlForge</a></div>
-                <div><a class="text-orange-600" href="https://thebomb.dev/">TheBomb.dev</a></div>
                 <div><a class="text-orange-600" href="https://bingo.krakero.dev/">Custom Bingo Boards</a></div>
                 <div><a class="text-orange-600" href="https://github.com/vmitchell85/laravel-cv">This Codebase</a></div>
             </div>
@@ -69,16 +61,15 @@
             <div class="my-6">
                 <h2>Experience</h2>
                 <div class="my-4 mx-2">
-                    <h3>Senior Consultant</h3>
+                    <h3>Senior Developer</h3>
                     <div class="flex items-center justify-between text-gray-500 -mt-1 text-sm">
-                        <div>Result Data Consulting, LTD / InnoSource Inc.</div>
-                        <div>2018 &mdash; Present</div>
+                        <div>Get Acceptd</div>
+                        <div>2022 &mdash; Present</div>
                     </div>
                     <ul class="text-sm mt-2 list-disc ml-6 text-gray-800">
-                        <li>Build and maintain web applications using primarily Laravel, VueJS, Tailwind CSS, InertiaJS</li>
-                        <li>Gather requirements for new features for web applications from stakeholders</li>
-                        <li>Write up new features, estimate, and delegate work to team of developers</li>
-                        <li>Forecast and plan upcoming projects and work for the development team</li>
+                        <li>Update, add, and improve features for the existing Laravel codebase</li>
+                        <li>Build and maintain Nova platform and features</li>
+                        <li>Self manage time and tasks to completion</li>
                     </ul>
                 </div>
                 <div class="my-4 mx-2">
@@ -92,6 +83,19 @@
                         <li>Build, customize, and deploy customer-speciﬁc web applications</li>
                         <li>Counsel and advise on social media involvement and issues</li>
                         <li>Troubleshoot and repair software and hardware computer issues</li>
+                    </ul>
+                </div>
+                <div class="my-4 mx-2">
+                    <h3>Senior Consultant</h3>
+                    <div class="flex items-center justify-between text-gray-500 -mt-1 text-sm">
+                        <div>Result Data Consulting, LTD / InnoSource Inc.</div>
+                        <div>2018 &mdash; 2022</div>
+                    </div>
+                    <ul class="text-sm mt-2 list-disc ml-6 text-gray-800">
+                        <li>Build and maintain web applications using primarily Laravel, VueJS, Tailwind CSS, InertiaJS</li>
+                        <li>Gather requirements for new features for web applications from stakeholders</li>
+                        <li>Write up new features, estimate, and delegate work to team of developers</li>
+                        <li>Forecast and plan upcoming projects and work for the development team</li>
                     </ul>
                 </div>
                 <div class="my-4 mx-2">
